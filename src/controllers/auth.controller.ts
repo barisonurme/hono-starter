046c5dac5618d20af40a13eb0b73cbc3b8a3592d
barisonurme <barisonurme@users.noreply.github.com>
2025-12-21 15:22:50 +0300

    refactor: improve request data validation in login handler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,7 +6,9 @@ import type { TLoginRoute } from "@/routes/auth/auth.route";
 import { authService } from "@/services/auth.service";
 
 export const login: TRouteHandler<TLoginRoute> = async (c) => {
-  const { email, passwordHash } = await c.req.json();
+  const data = c.req.valid("json");
+  const { email, passwordHash } = data;
+
   const user = await authService.validatePassword(email, passwordHash);
   return c.json(user, HttpStatusCodes.OK);
 };
